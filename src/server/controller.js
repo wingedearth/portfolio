69bf66a633f4ed00e56ac63d1e973985ba7d96eb
Andrew Anissi <aanissi@legacy.com>
2020-10-25 01:29:30 -0700

    feat: add express server

---
+++ src/server/controller.js
@@ -0,0 +1,5 @@
+const Controller = (req, res) => {
+	res.send('Welcome to the machine.');
+};
+
+export default Controller;
---
+++ src/server/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import router from './router';
+
+const port = process.env.PORT || 4000;
+const app = express();
+
+app.use(router);
+
+app.on('error', (err) => {
+	console.error(err?.message);
+});
+
+const server = app.listen(port, () => {
+	console.log(`A half-elven druid has conjured a server at port, ${port}.`);
+});
+
+process.on('SIGINT', () => {
+	server.close((err) => {
+		if (err) return process.exit(1);
+
+		process.exit(0);
+	});
+});
